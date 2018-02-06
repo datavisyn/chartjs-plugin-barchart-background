dadab067a3baf20cd25850e013a3c7ea240fa67b
sluger <stefan.luger@datavisyn.io>
2018-02-06 09:55:21 +0100

    include build

---
+++ build/Plugin.Barchart.Background.js
@@ -0,0 +1,61 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('chart.js')) :
+	typeof define === 'function' && define.amd ? define(['chart.js'], factory) :
+	(global.PluginBarchartBackground = factory(global.Chart));
+}(this, (function (Chart) { 'use strict';
+
+Chart = Chart && Chart.hasOwnProperty('default') ? Chart['default'] : Chart;
+
+var plugin = {
+	id: 'chartJsPluginBarchartBackground',
+
+	beforeDraw: function beforeDraw(chart, easingValue, options) {
+
+		var supportedTypes = ['boxplot', 'horizontalBoxplot', 'violin', 'horizontalViolin', 'bar', 'horizontalBar'];
+		if (!supportedTypes.find(chart.config.type)) {
+			console.warn('The type %s is not supported by this plugin', chart.config.type);
+			return;
+		}
+
+		var isHorizontal = chart.config.type.startsWith('horizontal') ? true : false;
+		var chartWidth = chart.chartArea.right - chart.chartArea.left;
+		var chartHeight = chart.chartArea.bottom - chart.chartArea.top;
+		var numGroups = Math.max(0, chart.data.datasets.map(function (d) {
+			return d.data.length;
+		}));
+
+		// push the current canvas state onto the stack
+		var ctx = chart.ctx;
+		ctx.save();
+
+		// set background color
+		ctx.fillStyle = options.color;
+
+		// draw rectangles
+		var groupWidth = void 0;
+		if (isHorizontal) {
+			groupWidth = chartHeight / numGroups;
+			var i = chart.chartArea.top;
+			while (i < chart.chartArea.bottom) {
+				ctx.fillRect(chart.chartArea.left, i, chartWidth, groupWidth);
+				i += groupWidth * 2;
+			}
+		} else {
+			groupWidth = chartWidth / numGroups;
+			var _i = chart.chartArea.left;
+			while (_i < chart.chartArea.right) {
+				ctx.fillRect(_i, chart.chartArea.top, groupWidth, chartHeight);
+				_i += groupWidth * 2;
+			}
+		}
+
+		// restore the saved state
+		ctx.restore();
+	}
+};
+
+Chart.pluginService.register(plugin);
+
+return plugin;
+
+})));
