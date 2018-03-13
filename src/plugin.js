76ebb714d686e05f0974aece23f813688d6274bf
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-03-13 17:13:35 +0100

    more conservative group rendering to avoid rounding errors

--- src/plugin.js
+++ src/plugin.js
@@ -25,46 +25,46 @@ const plugin = {
 	beforeInit: (chart) => {
 		if (!isSupported(chart.config.type)) {
 			console.warn('The type %s is not supported by this plugin', chart.config.type);
-			return;
 		}
 	},
 
 	beforeDraw: (chart, easingValue, options) => {
-		if (hasData(chart.config.data)) {
-			const pluginOptions = Object.assign({}, defaultOptions, options);
-			const isHorizontal = chart.config.type.startsWith('horizontal') ? true : false;
-			const chartWidth = chart.chartArea.right - chart.chartArea.left;
-			const chartHeight = chart.chartArea.bottom - chart.chartArea.top;
-			const numGroups = Math.max(...chart.config.data.datasets.map((d) => d.data.length));
-
-			// push the current canvas state onto the stack
-			const ctx = chart.ctx;
-			ctx.save();
-
-			// set background color
-			ctx.fillStyle = pluginOptions.color;
-
-			// draw rectangles
-			let groupWidth;
-			if (isHorizontal) {
-				groupWidth = chartHeight / numGroups;
-				let i = chart.chartArea.top;
-				while (i < chart.chartArea.bottom) {
-					ctx.fillRect(chart.chartArea.left, i, chartWidth, groupWidth);
-					i += groupWidth * 2;
-				}
-			} else {
-				groupWidth = chartWidth / numGroups;
-				let i = chart.chartArea.left;
-				while (i < chart.chartArea.right) {
-					ctx.fillRect(i, chart.chartArea.top, groupWidth, chartHeight);
-					i += groupWidth * 2;
-				}
-			}
-
-			// restore the saved state
-			ctx.restore();
+		if (!hasData(chart.config.data) || !isSupported(chart.config.type)) {
+			return;
 		}
+		const pluginOptions = Object.assign({}, defaultOptions, options);
+		const isHorizontal = chart.config.type.startsWith('horizontal');
+		const chartWidth = chart.chartArea.right - chart.chartArea.left;
+		const chartHeight = chart.chartArea.bottom - chart.chartArea.top;
+		const numGroups = Math.max(...chart.config.data.datasets.map((d) => d.data.length));
+
+		// push the current canvas state onto the stack
+		const ctx = chart.ctx;
+		ctx.save();
+
+		// set background color
+		ctx.fillStyle = pluginOptions.color;
+
+		// draw rectangles
+		let groupWidth;
+		if (isHorizontal) {
+			groupWidth = chartHeight / numGroups;
+			let acc = chart.chartArea.top;
+			for(let i = 0; i < numGroups; ++i) {
+				ctx.fillRect(chart.chartArea.left, acc, chartWidth, groupWidth);
+				acc += groupWidth * 2;
+			}
+		} else {
+			groupWidth = chartWidth / numGroups;
+			let acc = chart.chartArea.left;
+			for(let i = 0; i < numGroups; ++i) {
+				ctx.fillRect(acc, chart.chartArea.top, groupWidth, chartHeight);
+				acc += groupWidth * 2;
+			}
+		}
+
+		// restore the saved state
+		ctx.restore();
 	}
 };
 
